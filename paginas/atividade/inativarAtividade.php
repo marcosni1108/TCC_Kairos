4e364f813c057bf922a56ef7d0d1333b405f21c6
marcosni1108 <marcosbatista10@gmail.com>
2016-03-15 23:20:49 -0300

    Criação da pagina

---
+++ paginas/atividade/inativarAtividade.php
@@ -0,0 +1,14 @@
+<?php
+
+       include '../include/include_classes.php';
+                     $atividade = new atividade(); 
+
+                $id = $_GET['id'];
+                
+                    if ($atividade->delete($id)) {
+                    echo "<script>alert('Deletado com sucesso!')</script>";
+                    header("location:consultaDepartamento.php");
+                }
+
+
+
