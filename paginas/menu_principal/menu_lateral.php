3cafb107c684db69e198e6e3b5d15587aeda2d0c
marcosni1108 <marcosbatista10@gmail.com>
2016-03-23 21:12:43 -0300

    Mudança de icone

--- paginas/menu_principal/menu_lateral.php
+++ paginas/menu_principal/menu_lateral.php
@@ -6,7 +6,7 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="../menu_principal/menu_principal.php"><i class="fa fa-fw fa-bar-chart-o"></i> Dashboard</a>
+                        <a href="../menu_principal/menu_principal.php"><i class="fa fa-fw fa-home"></i> Home</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#funcionario"><i class="glyphicon glyphicon-user"></i> Funcionario <i class="fa fa-fw fa-caret-down"></i></a>
