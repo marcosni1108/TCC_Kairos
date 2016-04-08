f03d1532f1457a94a106fb892023bd17209b08cb
marcosni1108 <marcosbatista10@gmail.com>
2016-04-07 22:32:35 -0300

    Ajustes no endereço da data table

--- classes/model/tableAtivi.php
+++ classes/model/tableAtivi.php
@@ -3,13 +3,14 @@
 require 'atividade.php';
 
 $atividade = new atividade();
+$departamento = new departamento();
 $atividade->findAll();
 
 
 $i = 0;
 foreach ($atividade->findAll() as $key => $value) {
-    
-    $fields = array($value->nome, $value->idDepartamentoFK, $value->cnpj,
+    $dept = $departamento->find($value->idDepartamentoFK);
+    $fields = array($value->nome, $dept->nome, $value->cnpj,
         $value->unid_med, "<a href='editarAtividade.php?acao=editar&id=" . $value->id . "'><input type='submit' name='editar' class='btn btn-primary' value='Editar' style='margin-right:30px;margin-left:50px;'></a><a href='inativarAtividade.php?acao=deletar&id=" . $value->id . "' onclick='return confirm(\"Deseja realmente deletar?\")'><input type='submit' name='deletar' class='btn btn-danger' value='Deletar' style='margin-right:-60px;'></a>");
     $json_result["data"] [] = $fields;
 }
