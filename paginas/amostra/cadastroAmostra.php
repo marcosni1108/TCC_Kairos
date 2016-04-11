86a0510cce2bff0f824866827f557029fe4f9355
marcosni1108 <marcosbatista10@gmail.com>
2016-04-11 11:24:14 -0300

    Atualização da ajuda.

--- paginas/amostra/cadastroAmostra.php
+++ paginas/amostra/cadastroAmostra.php
@@ -19,7 +19,9 @@
                         <h1 class="page-header">
                             Registro de Amostra
                         </h1>          
-                        <div class="alert alert-info alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> <strong>ERRO!</strong> Usuario ou Senha invalidos! </div>
+                        <div class="alert alert-info alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> <strong>Ajuda:</strong>
+                           Para cadastrar amostra o usuário deve escolher o departamento, selecionar a atividade e clicar no botão cadastrar dados. <br>
+                           <strong>Obs:</strong> A pagina será redirecionada para que o usuário coloque as informações adicionais.  </div>
                         <div class="row">
                             <div class="form-group col-lg-4">
                                 <label for="cnpj">CNPJ</label>
