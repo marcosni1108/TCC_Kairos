e0c2b0fe9ff6055d87fcae6ad01971404f48d473
marcosni1108 <marcosbatista10@gmail.com>
2016-04-12 09:26:04 -0300

    Ajuste na colunas de lider e gerente

--- classes/model/tableDept.php
+++ classes/model/tableDept.php
@@ -5,14 +5,20 @@ require 'endereco.php';
 $departamento = new departamento();
 $departamento->findAll();
 $endereco = new endereco();
+$usuario = new funcionario();
 
 $i = 0;
 foreach ($departamento->findAll() as $key => $value) {
     $idEnd = $value->idEnderecoFK;
     $end = $endereco->find($idEnd);
-    $fields = array($value->nome, $end->endereco." Nº ".$end->numero, $value->cnpj, $value->lider,
-        $value->gerente, "<a href='editarDepartamento.php?acao=editar&idDept=" . $value->id . "'><input type='submit' name='editar' class='btn btn-primary' value='Editar' style='margin-right:30px;margin-left:50px;'></a><a href='inativarDepartamento.php?acao=deletar&idDept=" . $value->id . "' onclick='return confirm(\"Deseja realmente deletar?\")'><input type='submit' name='deletar' class='btn btn-danger' value='Deletar' style='margin-right:-50px;'></a>");
-    $json_result["data"] [] = $fields;
+    $lider = $usuario->find($value->lider);
+    $gerente = $usuario->find($value->gerente);
+    $fields = array($value->nome, $end->endereco." Nº ".$end->numero, $value->cnpj, $lider->nome,
+        $gerente->nome, "<a href='editarDepartamento.php?acao=editar&idDept=" . $value->id . "'>"
+        . "<input type='submit' name='editar' class='btn btn-primary' value='Editar' style='margin-right:20px;margin-left:20px;'></a>"
+        . "<a href='inativarDepartamento.php?acao=deletar&idDept=" . $value->id . "' onclick='return confirm(\"Deseja realmente deletar?\")'>"
+        . "<input type='submit' name='deletar' class='btn btn-danger' value='Deletar' style='margin-right:-20px;'></a>");
+         $json_result["data"] [] = $fields;
 }
 $JSON = json_encode($json_result);
 
