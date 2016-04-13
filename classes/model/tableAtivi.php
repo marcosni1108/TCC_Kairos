1536a0b6fe5deca23eaf47360da8d3e096c78cb7
marcosni1108 <marcosbatista10@gmail.com>
2016-04-13 15:17:18 -0300

    Correções de bugs

--- classes/model/tableAtivi.php
+++ classes/model/tableAtivi.php
@@ -1,16 +1,21 @@
 <?php
 
 require 'atividade.php';
+require 'departamento.php';
+require 'endereco.php';
 
 $atividade = new atividade();
 $departamento = new departamento();
+$endereco = new endereco();
 $atividade->findAll();
 
 
 $i = 0;
 foreach ($atividade->findAll() as $key => $value) {
     $dept = $departamento->find($value->idDepartamentoFK);
-    $fields = array($value->nome, $dept->nome, $value->cnpj,
+    
+    $end = $endereco->find($dept->idEnderecoFK);
+    $fields = array($value->nome, $dept->nome, $end->cnpj,
         $value->unid_med, "<a href='editarAtividade.php?acao=editar&id=" . $value->id . "'><input type='submit' name='editar' class='btn btn-primary' value='Editar' style='margin-right:30px;margin-left:50px;'></a><a href='inativarAtividade.php?acao=deletar&id=" . $value->id . "' onclick='return confirm(\"Deseja realmente deletar?\")'><input type='submit' name='deletar' class='btn btn-danger' value='Deletar' style='margin-right:-60px;'></a>");
     $json_result["data"] [] = $fields;
 }
