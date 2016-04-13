a146e544a8e1903ef255dc5e9952eacc91c1d67d
marcosni1108 <marcosbatista10@gmail.com>
2016-04-13 15:11:29 -0300

    Correções de bugs

--- paginas/funcionario/inativarFuncionario.php
+++ paginas/funcionario/inativarFuncionario.php
@@ -4,10 +4,16 @@
                      $funcionario = new funcionario(); 
 
                 $matricula = $_GET['id'];
-                
-                    if ($funcionario->delete($matricula)) {
-                    echo "Deletado com sucesso!";
-                    header("location:consultaFuncionario.php");
+                $deletar = $funcionario->delete($matricula);
+                    if ($deletar===true) {
+                    echo "<script type='text/javascript' charset='utf-8'>alert('Deletado com sucesso!');"
+                         .  "window.location='./consultaFuncionario.php';</script>";
+                   // header("location:consultaFuncionario.php");
+                }
+                 else if ($deletar==='1451') {
+                    echo "<script type='text/javascript' charset='utf-8'>alert('Não foi possivel deletar, verifique se existem associações com esse funcionário!');"
+                      .  "window.location='./consultaFuncionario.php';</script>";
+                    //header("location:consultaEndereco.php");
                 }
 
 
