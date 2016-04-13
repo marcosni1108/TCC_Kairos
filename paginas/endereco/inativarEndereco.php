0347e8304fd69134704c1b188ea4a90aae1b6ebc
marcosni1108 <marcosbatista10@gmail.com>
2016-04-13 15:08:24 -0300

    Correções de bugs

--- paginas/endereco/inativarEndereco.php
+++ paginas/endereco/inativarEndereco.php
@@ -4,10 +4,16 @@
                      $endereco = new endereco(); 
 
                 $id = $_GET['id'];
-                
-                    if ($endereco->delete($id)) {
-                    echo "<script>alert('Deletado com sucesso!')</script>";
-                    header("location:consultaEndereco.php");
+                $deletar =  $endereco->delete($id);
+                    if ($deletar===true) {
+                    echo "<script type='text/javascript' charset='utf-8'>alert('Deletado com sucesso!');"
+                         .  "window.location='./consultaEndereco.php';</script>";
+                   // header("location:consultaEndereco.php");
+                }
+                  else if ($deletar==='1451') {
+                    echo "<script type='text/javascript' charset='utf-8'>alert('Não foi possivel deletar, verifique se existem associações com esse endereço!');"
+                      .  "window.location='./consultaEndereco.php';</script>";
+                    //header("location:consultaEndereco.php");
                 }
 
 
