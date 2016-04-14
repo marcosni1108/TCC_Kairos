aacc41b3667c3b48bb75213e04917d6d3ebfdd9d
marcosni1108 <marcosbatista10@gmail.com>
2016-04-14 10:38:06 -0300

    Correções.

--- paginas/include/include_js.php
+++ paginas/include/include_js.php
@@ -5,13 +5,12 @@
 <script src="../../js/bootstrap.min.js"></script>
 
 <!-- Morris Charts JavaScript -->
-<script src="../../js/plugins/morris/raphael.min.js"></script>
+<!--<script src="../../js/plugins/morris/raphael.min.js"></script>
 <script src="../../js/plugins/morris/morris.min.js"></script>
-<script src="../../js/plugins/morris/morris-data.js"></script>
+<script src="../../js/plugins/morris/morris-data.js"></script>-->
 
 
 <script src="../../js/jquery-1.12.0.min.js"></script>
 <script src="../../js/jquery.dataTables.min.js"></script>
  <script src="../../js/data.js"></script>
- <script src="../../js/bootstrap-datetimepicker.min.js"></script>
- <script src="../../js/datepicker.js"></script>
+
