cd605c78b235100fc66e9fad034905412f2d9c8d
marcosni1108 <marcosbatista10@gmail.com>
2016-04-14 10:34:50 -0300

    Correções.

--- paginas/include/include_css.php
+++ paginas/include/include_css.php
@@ -5,11 +5,9 @@
     <link href="../../css/sb-admin.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="../../css/plugins/morris.css" rel="stylesheet">
+<!--   // <link href="../../css/plugins/morris.css" rel="stylesheet">-->
 
     <!-- Custom Fonts -->
     <link href="../../font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    
-    
-    <link href="../../font-awesome/css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css">
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
