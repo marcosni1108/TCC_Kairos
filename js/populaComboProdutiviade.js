ab25094ff33bb838f0d71bc09c4fd08fc6945a68
marcosni1108 <marcosbatista10@gmail.com>
2016-04-15 11:26:03 -0300

    Correções.

---
+++ js/populaComboProdutiviade.js
@@ -0,0 +1,90 @@
+// popula o combo de departamentos quando a pagina é carregada.
+	$('#cmbDepartamento').ready(function(){
+            
+            $('#btnIniciar').prop("disabled",true);
+                $('#btnParar').prop("disabled",true);
+                $('#btnPausar').prop("disabled",true);
+		//chama o serviço que consulta os departamentos.
+		$.getJSON('../../classes/model/consulta.php?opcao=departamento', function (dados){ 
+		//verifica o json e cria um options.
+		   if (dados.length > 0){	
+			  var option = '<option value="">Selecione o Departamento</option>';
+			  $.each(dados, function(i, obj){
+				  option += '<option value="'+obj.id+'">'+obj.nome+'</option>';
+			  })
+			 
+		   }else{
+			  Reset();
+			
+		   }
+		   $('#cmbDepartamento').html(option).show(); 
+                  $('#cnpj').val('');
+		})
+	})
+        
+        //quando é alterado o departamento
+	$('#cmbDepartamento').change(function(e){
+		var end = $('#cmbDepartamento').val();
+		
+                //desabilita o botão cadastrar até que o combos serem populados.
+		$('#btnIniciar').prop("disabled",true);
+                $('#btnParar').prop("disabled",true);
+                $('#btnPausar').prop("disabled",true);
+                //chama o serviço que traz o cpnj do departamento.
+		$.getJSON('../../classes/model/consulta.php?opcao=cnpj&tipo=atividade&valor='+end, function (dados){
+			
+			if (dados){ 	
+				var value = '';
+                                value = dados.cnpj;
+                       
+			}else{
+				Reset();
+				
+			}
+                        
+                        //insere o cnpj no input txt
+			$('#cnpj').val(value)
+                        
+                        //chama o serviço que lista as atividades relacionadas ao departamento.
+                        $.getJSON('../../classes/model/consulta.php?opcao=atividade&valor='+end, function (dados1){
+			
+                         if (dados1.length > 0){	
+                            var option = '<option value="">Selecione o Atividade</option>';
+                            $.each(dados1, function(i, obj){
+                                    option += '<option value="'+obj.id+'">'+obj.nome+'</option>';
+                            })
+                        }
+                        else{
+				Reset("atividade");
+				
+			}
+			//popula o combo de atividades.
+                          $('#cmbAtividade').html(option).show(); 
+                          
+                          //habilita o botões.
+                          $('#btnIniciar').prop("disabled",false);
+                           $('#btnParar').prop("disabled",false);
+                            $('#btnPausar').prop("disabled",false);
+		})
+                        
+		})
+	})
+        
+
+        
+        
+	function Reset(tipo){
+		$('#endereco').empty().append('<option>Carregar Endereço</option>>');
+		$('#cnpj').val('');
+                $('#cmbDepartamento').empty().append('<option>Carregar Departamento</option>>');
+                if(tipo==='atividade'){
+                    alert("Departamento sem Atividades");
+                    location.href='cadastroAmostra.php'; 
+                     $('#cmbAtividade').empty().append('<option>Carregar Atividades</option>>');                                                    
+                 }
+               
+		
+	}
+
+
+
