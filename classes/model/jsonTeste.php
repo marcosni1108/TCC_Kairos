eccb17409ff76230eb55fca0be43eb43345faaf1
marcosni1108 <marcosbatista10@gmail.com>
2016-05-02 14:20:59 -0300

    Serviço para gerar Json Teste

--- classes/model/jsonTeste.php
+++ classes/model/jsonTeste.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'endereco.php';
+require 'produtividade.php';
 $opcao = isset($_GET['opcao']) ? $_GET['opcao'] : '';
 $valor = isset($_GET['valor']) ? $_GET['valor'] : '';
 if (! empty($opcao)){	
@@ -12,6 +13,11 @@ if (! empty($opcao)){
 				echo getAllEnd($valor);
 				break;
 			}
+                case 'findProd':
+			{
+				echo findProd();
+				break;
+			}
         }
     
 }
@@ -20,6 +26,13 @@ function getAllEnd($id){
         $endereco = new endereco();
         $teste = $endereco->find($id);
         $JSON = json_encode($teste);
+        echo $JSON;
+	
+}
+function findProd(){
+        $prod = new produtividade();
+        $result = $prod->findProdTotal();
+        $JSON = json_encode($result);
         echo $JSON;
 	
 }
