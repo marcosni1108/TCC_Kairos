b8da84e025784da5aeceb167aeefcb65c4c5a645
Ricardo <Ricardo@RCO>
2016-05-21 19:38:20 -0300

    Correções e Ajustes na classe turno

--- classes/model/turno.php
+++ classes/model/turno.php
@@ -9,10 +9,18 @@ class turno {
             case 10: return "2";
             case 11: return "3";
             case 12: return "4";
-            case 14: return "5";
-            case 16: return "6";
-            case 17: return "7";
-            case 18: return "8";
+            case 13: return "5";    
+            case 14: return "6";
+            case 15: return "7";    
+            case 16: return "8";
+            case 17: return "9";
+            case 18: return "10";
+            case 19: return "11";
+            case 20: return "12";
+            case 21: return "13";
+            case 22: return "14";
+            case 23: return "15";
+            case 24: return "16";    
         }
         
     }
