4a1e386cb5e86290670b726b52f5708509872622
12131502008 <12131502008@14LAB115.umc.br>
2016-09-15 20:43:28 -0300

    teste

--- classes/model/config.php
+++ classes/model/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'mysql.hostinger.com.br');
-define('DB_NAME', 'u825403659_kairo');
-define('DB_USER', 'u825403659_root');
+define('DB_NAME', 'u649562998_kairo');
+define('DB_USER', 'u649562998_root');
 define('DB_PASS', 'ricardo13');
