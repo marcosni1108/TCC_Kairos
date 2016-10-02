4215fdc5352a4df703ca7d13121600b60dd4a214
EDU <EDU@EDU-PC.Home>
2016-10-02 16:43:03 -0300

    Config.php modificado -  adicionado meu IP local no IF

---
+++ classes/graficos/GraficoAtividade.php
@@ -0,0 +1,23 @@
+<?php
+set_include_path(dirname(__FILE__)."/../model");
+require_once 'atividade.php';
+$atividade = new atividade();
+$arrayFunc = $atividade->findProdTotalAtividade($_SESSION['departamento']);
+$bln = array();
+$bln['name'] = 'Atividade';
+$rows['name'] = 'Produção';
+if ($arrayFunc) {
+
+    foreach ($arrayFunc as $key => $value) {
+
+
+        $bln['data'][] = $value->nome;
+        $rows['data'][] = $value->prod;
+    }
+    
+$rslt = array();
+array_push($rslt, $bln);
+array_push($rslt, $rows);
+echo json_encode($rslt, JSON_NUMERIC_CHECK);
+
+}
