dfb34ccf21db02a86bfee8df136b3655576c3817
Ricardo <Ricardo@RCO>
2016-10-02 14:40:22 -0300

    Inclusão do condicional para identificar o servidor de uso.

--- classes/model/config.php
+++ classes/model/config.php
@@ -1,7 +1,14 @@
 <?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'kairos');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+$servidor = $_SERVER['SERVER_NAME'];
+if($servidor=='localhost'){
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'kairos');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+}else{
+    define('DB_HOST', 'mysql.hostinger.com.br');
+    define('DB_NAME', 'u825403659_kairo');
+    define('DB_USER', 'u825403659_root');
+    define('DB_PASS', 'ricardo13');
+}
 
