563bedd2ec2af12a2fde858ccefc71c5aa9c6dd8
Ricardo <Ricardo@RCO>
2016-10-24 22:31:01 -0200

    Ajustes nomenclatura classes.

--- paginas/include/include_classes.php
+++ paginas/include/include_classes.php
@@ -1,12 +1,6 @@
 <?php
 
-function __autoload($class_name) {
-   //  $controller = file_exists('../../classes/controller/' . $class_name . '.php');
-     //$model = file_exists('../../classes/model/' . $class_name . '.php');
-     
-       
-   // if ($model) {
-        require_once '../../classes/model/' . $class_name . '.php';
-  //  }
-   
+function __autoload($class) {
+    $$class_name = strtolower($class);
+    require_once '../../classes/model/' . $class_name . '.php';
 }
