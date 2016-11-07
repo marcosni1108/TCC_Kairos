910f80d6e0d93bc1641770ac0d2ccb95848ed189
rsantana93 <rikardinhosousa98@gmail.com>
2016-11-07 11:04:23 -0200

    Update login.php
    Adição do titulo para pagina

--- paginas/login/login.php
+++ paginas/login/login.php
@@ -2,7 +2,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
-        <title></title>
+        <title>Kairos - Login</title>
         <?php
         session_start();
         include "../include/include_css.php";
