63f25b306b6b42f376cd355e57452b374d59137f
rsantana93 <rikardinhosousa98@gmail.com>
2016-11-22 15:37:15 -0200

    Update GraficoProdPorHoraAtiv.php
    inclusão da biblioteca matrial.min.js

--- paginas/graficos/GraficoProdPorHoraAtiv.php
+++ paginas/graficos/GraficoProdPorHoraAtiv.php
@@ -104,4 +104,5 @@
 <script src="../../js/jquery-1.12.0.min.js"></script>
 <script src="../../js/bootstrap.min.js"></script>
 <script src="../../js/populaComboGraficoFunc.js"></script>
+<script src="../../js/material.min.js"></script>
 </html>
