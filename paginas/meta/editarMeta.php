8fce72ce47482686760fc97f9cda4ee4abbaba3c
rsantana93 <rikardinhosousa98@gmail.com>
2016-11-22 13:05:38 -0200

    Update editarMeta.php
    Ajuste na diagramação da pagina.

--- paginas/meta/editarMeta.php
+++ paginas/meta/editarMeta.php
@@ -42,7 +42,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-8">
+                    <div class="col-lg-12">
                         <div class="panel panel-default">
                             <!-- Conteudo dentro de wrapper -->
                             <div class="panel-body">
