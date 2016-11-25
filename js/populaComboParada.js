18156d63149921de2bf2dea2869af53f1eecc060
Ricardo <Ricardo@RCO>
2016-11-25 00:26:08 -0200

    Correção de erros na logica.

--- js/populaComboParada.js
+++ js/populaComboParada.js
@@ -1,40 +1,47 @@
-function Reset(a) {
-    $("#endereco").empty().append("<option>Carregar Endereço</option>>"), $("#cnpj").val(""), $("#cmbDepartamento").empty().append("<option>Carregar Departamento</option>>"), "atividade" === a && (alert("Departamento sem Atividades"), location.href = "cadastroAmostra.php", $("#cmbAtividade").empty().append("<option>Carregar Atividades</option>>")), "Dept" === a && (alert("Filial sem departamento"), $("#cnpj").val(""), $("#cmbDepartamento").empty().append("<option>Carregar Departamento</option>>"))
-}
-$("#cnpj").ready(function () {
-    $.getJSON("../../classes/model/consulta.php?opcao=allCNPJ", function (a) {
-        if (a.length > 0) {
-            var b = '<option value="">Selecione a Filial</option>';
-            $.each(a, function (a, c) {
-                b += '<option value="' + c.id + '">' + c.nomeFilial + "</option>"
-            })
-        } else
+//carrega todos os cnpj
+$('#cnpj').ready(function () {
+    //chama o serviço que consulta os departamentos.
+    // $('#btnCadastrar').prop("disabled",true);
+    $.getJSON('../../classes/model/consulta.php?opcao=allCNPJ', function (dados) {
+        //verifica o json e cria um options.
+        if (dados.length > 0) {
+            var option = '<option value="">Selecione a Filial</option>';
+            $.each(dados, function (i, obj) {
+                option += '<option value="' + obj.id + '">' + obj.nomeFilial + '</option>';
+            });
+        } else {
             Reset();
-        $("#cnpj").html(b).show()
-    })
-}), $("#cnpj").change(function (a) {
-    $("#btnCadastrar").prop("disabled", !0);
-    var b = $("#cnpj").val();
-    $.getJSON("../../classes/model/consulta.php?opcao=FindDeptCnpj&valor=" + b, function (a) {
-        if (a.length > 0) {
-            var b = '<option value="">Selecione o Departamento</option>';
-            $.each(a, function (a, c) {
-                b += '<option value="' + c.id + '">' + c.nome + "</option>"
-            })
-        } else
-            Reset("Dept");
-        $("#cmbDepartamento").html(b).show(), $("#btnCadastrar").prop("disabled", !1)
-    })
-}), $("#cmbDepartamento").change(function (a) {
-    var b = $("#cmbDepartamento").val();
-    $("#btnCadastrar").prop("disabled", !0), $.getJSON("../../classes/model/consulta.php?opcao=atividade&valor=" + b, function (a) {
-        if (a.length > 0) {
-            var b = '<option value="">Selecione o Atividade</option>';
-            $.each(a, function (a, c) {
-                b += '<option value="' + c.id + '">' + c.nome + "</option>"
-            })
-        } else
-            Reset("atividade");
-        $("#cmbAtividade").html(b).show(), $("#btnCadastrar").prop("disabled", !1)
-    })
+        }
+        $('#cnpj').html(option).show();
+    });
 });
+
+// popula o combo de departamentos quando a pagina é carregada.
+$('#cnpj').change(function (e) {
+    $('#btnCadastrar').prop("disabled", true);
+    var idCNPJ = $('#cnpj').val();
+    //chama o serviço que consulta os departamentos.
+    $.getJSON('../../classes/model/consulta.php?opcao=FindDeptCnpj&valor=' + idCNPJ, function (dados) {
+        //verifica o json e cria um options.
+        if (dados.length > 0) {
+            var option = '<option value="">Selecione o Departamento</option>';
+            $.each(dados, function (i, obj) {
+                option += '<option value="' + obj.id + '">' + obj.nome + '</option>';
+            });
+        } else {
+            Reset("Dept");
+        }
+        $('#cmbDepartamento').html(option).show();
+        $('#btnCadastrar').prop("disabled", false);
+    });
+});
+function Reset(tipo) {
+    $('#endereco').empty().append('<option>Carregar Endereço</option>>');
+    $('#cnpj').val('');
+    $('#cmbDepartamento').empty().append('<option>Carregar Departamento</option>>');
+    if (tipo === 'Dept') {
+        alert("Filial sem departamento");
+        $('#cnpj').val('');
+        $('#cmbDepartamento').empty().append('<option>Carregar Departamento</option>>');
+    }
+}
